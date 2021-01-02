591418d71ea65b47df41ffb57cd189ad180d0cc6
Mercy Tum <mercytum@gmail.com>
2021-01-02 20:51:04 +0300

    A Todo List App in Vue JS

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the ToDoList App v1.0.0. I made this from the Vue crash course on YouTube by  <a href="https://www.youtube.com/watch?v=Wy9q22isx3U">Traversy Media</a> </p>
+  </div>
+</template>
